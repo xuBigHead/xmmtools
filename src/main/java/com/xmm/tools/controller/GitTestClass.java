2e57a65c4b3d52e5628ae4d0e03f58f65b521b3b
xuBigHead <1390589756@qq.com>
2019-10-22 11:43:21 +0800

    Merge branch 'dev'
    # Conflicts:
    #	src/main/java/com/xmm/tools/controller/GitTestClass.java

--- src/main/java/com/xmm/tools/controller/GitTestClass.java
+++ src/main/java/com/xmm/tools/controller/GitTestClass.java
@@ -1,9 +1,16 @@
 package com.xmm.tools.controller;
 
+import org.junit.Test;
+import org.springframework.web.bind.annotation.PutMapping;
+
 public class GitTestClass {
     public static void main(String[] args) {
         //System.out.println("11点33分");
         //System.out.println("11点34分");
         System.out.println("11点35分");
+    }
+    @Test
+    public void test000(){
+        System.out.println("test000");
     }
 }
