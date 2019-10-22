661ba7be7cfded515ceb3050b09a2ac8ec188d21
xuBigHead <1390589756@qq.com>
2019-10-22 11:10:17 +0800

    2019年10月22日V-1

--- src/main/java/com/xmm/tools/controller/GitTestClass.java
+++ src/main/java/com/xmm/tools/controller/GitTestClass.java
@@ -2,6 +2,6 @@ package com.xmm.tools.controller;
 
 public class GitTestClass {
     public static void main(String[] args) {
-        System.out.println("11点04分");
+        System.out.println("11点10分");
     }
 }
