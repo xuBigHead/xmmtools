3a62ebc600e93072a05ae803e42cbc542ccc22dc
xuBigHead <48990224+xuBigHead@users.noreply.github.com>
2019-10-22 11:36:30 +0800

    Update GitTestClass.java
    others changed

--- src/main/java/com/xmm/tools/controller/GitTestClass.java
+++ src/main/java/com/xmm/tools/controller/GitTestClass.java
@@ -3,5 +3,6 @@ package com.xmm.tools.controller;
 public class GitTestClass {
     public static void main(String[] args) {
         System.out.println("11点33分");
+        System.out.println("11点34分");
     }
 }
