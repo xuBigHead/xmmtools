9f229d4a7ff88a2cc9003503fcfa23760ce28c89
xuBigHead <1390589756@qq.com>
2019-10-23 21:20:25 +0800

    2019年10月23日

--- src/main/java/com/xmm/tools/controller/GitTestClass2.java
+++ src/main/java/com/xmm/tools/controller/GitTestClass2.java
@@ -4,20 +4,23 @@ import org.junit.Test;
 
 public class GitTestClass2 {
     @Test
-    public void test222(){
+    public void test222() {
         System.out.println("test000");
     }
+
     @Test
-    public void test555(){
+    public void test555() {
         System.out.println("test555");
     }
-        @Test
-    public void test444(){
+
+    @Test
+    public void test444() {
         System.out.println("test000");
         System.out.println("2019年10月23日");
     }
-         @Test
-    public void test555(){
+
+    @Test
+    public void test666() {
         System.out.println("test000");
         System.out.println("2019年10月23日");
     }
