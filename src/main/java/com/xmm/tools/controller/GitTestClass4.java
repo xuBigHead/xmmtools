0777747bc57afccf42b2a81f6df7b9fe297b46a8
xuBigHead <1390589756@qq.com>
2019-10-27 20:46:48 +0800

    Merge branch 'master' of https://github.com/xuBigHead/xmmtools
    # Conflicts:
    #	src/main/java/com/xmm/tools/controller/GitTestClass2.java
    #	src/main/java/com/xmm/tools/controller/GitTestClass4.java

--- src/main/java/com/xmm/tools/controller/GitTestClass4.java
+++ src/main/java/com/xmm/tools/controller/GitTestClass4.java
@@ -4,12 +4,14 @@ import org.junit.Test;
 
 public class GitTestClass4 {
     @Test
-    public void test111(){
+    public void test111() {
         System.out.println("21点15分30s");
         System.out.println("2121");
-         System.out.println("212311111");
-         System.out.println("21230000");
+        System.out.println("212311111");
+        System.out.println("21230000");
         System.out.println("21230000");
         //System.out.println("2122");
+        System.out.println("21230000");
+        System.out.println("21230000");
     }
 }
